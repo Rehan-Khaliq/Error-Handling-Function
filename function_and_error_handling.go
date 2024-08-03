c739a69c9c5d93e747ff6c03df4bb2e30d616541
Rehan-Khaliq <rehankhaliqkwl@gmail.com>
2024-08-03 11:24:31 +0500

    Add files via upload

---
+++ function_and_error_handling.go
@@ -0,0 +1,80 @@
+package main
+
+import "fmt"
+
+func simpleFunciton() {
+	fmt.Println("Simple Function:")
+}
+
+func add(a, b int) int {
+	return a + b
+}
+func multiply(x, y int) int {
+	return x * y
+}
+
+func divided(a, b int) (result int) { // we stored the output into result function and return result
+	result = a / b
+	return
+}
+
+func main() {
+	fmt.Println("We are learning functions today:")
+	simpleFunciton()
+	ans := add(3, 4)
+	fmt.Println("The Addition Of 2 Number Is:", ans)
+	// Now by applying 2nd Method
+	fmt.Println("By Adding Two Numbers:\n", add(4, 4))
+	fmt.Println("The Answer Of Multipication Function is:", multiply(3, 9))
+	// By calling didvided function
+	fmt.Println("The Divison Of Two Number Is: ", divided(3, 2))
+}
+
+// Error Handling In Fuction
+package main
+
+import "fmt"
+
+func divide(a, b int) int { // The issue is that our answer should be in float but comes in int, now solve
+	return a / b
+}
+
+// Now by Solving float value issue
+func dvided(a, b float64) float64 {
+	return a / b
+}
+
+// Now making function that is divided by zero
+func division(a, b float64) float64 { // if number is divided by 0 answer comes in infinity
+	return a / b
+}
+
+// We don't want to get answer in infinity now we handle this
+func inf_division(a, b float64) float64 {
+	if b == 0 {
+		return 0
+	}
+	return a / b
+}
+
+// Now we work for error handling so
+func err_division(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("Denominator Must Not Be Zero:")
+	}
+	return a / b, nil // Therefore we use nil because this part is completly correct
+}
+
+func main() {
+	fmt.Println("Started Error Handling In Function:")
+	fmt.Println("The Division Of Two Number Is:", divide(10, 4))
+	fmt.Println("Now The Answer would become in float after solving issue:", dvided(10, 4))
+	fmt.Println("The Answer After Divided By '0':", division(10, 0))  // Answer Comes In Infinity
+	fmt.Println("By Handling Infinity Problem:", inf_division(10, 4)) // Function Works Properly
+	// The Lower Given Part Is A Part Of err_division function
+    // The Lower Given "_" represents the Declaration Of "error" 
+    // we use "_" on any place where function returns some value and we want to ignore them
+    // you can use "_" where you want to ignore the output somewhere
+	ans, _ := err_division(10, 0)
+	fmt.Println("The Answer after solving by error", ans)
+}
